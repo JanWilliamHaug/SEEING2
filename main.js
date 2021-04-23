fdd168eb5234cbf870a36931a6faf7da1c0fbb17
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2021-04-23 05:42:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -34,34 +34,4 @@ const myConsole = new console.Console(fs.createWriteStream('./output.txt'))
 
 
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-const bucketName = 'seeingi2';
-const filename = 'output.txt';
 
-// Imports the Google Cloud client library
-const {Storage} = require('@google-cloud/storage');
-
-// Creates a client
-const storage = new Storage();
-
-async function uploadFile() {
-  // Uploads a local file to the bucket
-  await storage.bucket(bucketName).upload(filename, {
-    // Support for HTTP requests made with `Accept-Encoding: gzip`
-    gzip: true,
-    // By setting the option `destination`, you can change the name of the
-    // object you are uploading to a bucket.
-    metadata: {
-      // Enable long-lived HTTP caching headers
-      // Use only if the contents of the file will never change
-      // (If the contents will change, use cacheControl: 'no-cache')
-      cacheControl: 'public, max-age=31536000',
-    },
-  });
-
-  console.log(`${filename} uploaded to ${bucketName}.`);
-}
-
-uploadFile();
